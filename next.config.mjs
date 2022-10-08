cfaeb5059bf028cb82fcc9b1f605e9b7a836829f
Kacper <kpawlak2000@gmail.com>
2022-10-08 16:28:33 +0200

    adjust config

--- next.config.mjs
+++ next.config.mjs
@@ -35,6 +35,10 @@ export default defineNextConfig({
         source: '/songs/:songId',
         destination: '/',
       },
+      {
+        source: '/artist/:artistId',
+        destination: '/',
+      },
     ];
   },
 });
